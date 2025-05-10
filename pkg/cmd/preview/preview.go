175767cc59db46e2ac235859af6c12a57248bf58
Kynan Ware <47394200+BagToad@users.noreply.github.com>
2025-05-10 11:54:32 -0600

    doc(preview): add long description

--- pkg/cmd/preview/preview.go
+++ pkg/cmd/preview/preview.go
@@ -1,6 +1,7 @@
 package preview
 
 import (
+	"github.com/MakeNowJust/heredoc"
 	cmdPrompter "github.com/cli/cli/v2/pkg/cmd/preview/prompter"
 	"github.com/cli/cli/v2/pkg/cmdutil"
 	"github.com/spf13/cobra"
@@ -10,6 +11,10 @@ func NewCmdPreview(f *cmdutil.Factory) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "preview <command>",
 		Short: "Execute previews for gh features",
+		Long: heredoc.Doc(`
+			Preview commands are for testing, demonstrative, and development purposes only.
+			They should be considered unstable and can change at any time.
+		`),
 	}
 
 	cmdutil.DisableAuthCheck(cmd)
