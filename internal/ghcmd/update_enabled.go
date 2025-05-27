dd4095d0163a8218d3fb6e37235f5eb7ece434fe
Kynan Ware <47394200+BagToad@users.noreply.github.com>
2025-05-27 13:05:47 -0600

    doc(update): improve comments on updaterEnabled

--- internal/ghcmd/update_enabled.go
+++ internal/ghcmd/update_enabled.go
@@ -2,10 +2,17 @@
 
 package ghcmd
 
-// updaterEnabled is a statically linked build property set in gh formula within homebrew/homebrew-core
-// used to control whether users are notified of newer GitHub CLI releases.
-// This needs to be set to 'cli/cli' as it affects where update.CheckForUpdate() checks for releases.
-// It is unclear whether this means that only homebrew builds will check for updates or not.
-// Development builds leave this empty as impossible to determine if newer or not.
-// For more information, <https://github.com/Homebrew/homebrew-core/blob/master/Formula/g/gh.rb>.
+// `updateable` is a build tag set in the gh formula within homebrew/homebrew-core
+// and is used to control whether users are notified of newer GitHub CLI releases.
+//
+// Currently, updaterEnabled needs to be set to 'cli/cli' as it affects where
+// update.CheckForUpdate() checks for releases. It is unclear to what extent
+// this updaterEnabled is being used by unofficial forks or builds, so we decided
+// to leave it available for injection as a string variable for now.
+//
+// Development builds do not generate update messages by default.
+//
+// For more information, see:
+// - the Homebrew formula for gh: <https://github.com/Homebrew/homebrew-core/blob/master/Formula/g/gh.rb>.
+// - a discussion about adding this build tag: <https://github.com/cli/cli/pull/11024#discussion_r2107597618>.
 var updaterEnabled = "cli/cli"
