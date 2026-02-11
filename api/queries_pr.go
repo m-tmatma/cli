a8655bcda9261f462f6bc37f97b157fd1893c7c3
Kynan Ware <47394200+BagToad@users.noreply.github.com>
2026-02-11 13:55:57 -0700

    Include bot logins in login-based reviewer mutation guard

--- api/queries_pr.go
+++ api/queries_pr.go
@@ -618,10 +618,10 @@ func CreatePullRequest(client *Client, repo *Repository, params map[string]inter
 
 	// Request reviewers using either login-based (github.com) or ID-based (GHES) mutation
 	userLogins, hasUserLogins := params["userReviewerLogins"].([]string)
-	botLogins, _ := params["botReviewerLogins"].([]string)
+	botLogins, hasBotLogins := params["botReviewerLogins"].([]string)
 	teamSlugs, hasTeamSlugs := params["teamReviewerSlugs"].([]string)
 
-	if hasUserLogins || hasTeamSlugs {
+	if hasUserLogins || hasBotLogins || hasTeamSlugs {
 		// Use login-based mutation (RequestReviewsByLogin) for github.com
 		err := RequestReviewsByLogin(client, repo, pr.ID, userLogins, botLogins, teamSlugs, true)
 		if err != nil {
